10317272cc9ac983c4687f23a52d7f08c12c940c
Summaiyafatima <161141307+Summaiyafatima@users.noreply.github.com>
2024-02-24 20:47:12 +0530

    Add files via upload

---
+++ Firstprogramtest.py
@@ -0,0 +1,36 @@
+print("my name is summaiya .")
+print( "my age is ..")
+print (23)
+print(23+25)
+print("Hello ,World!")
+CODING = ["C","JAVA","PYTHON"]
+x,y,z = CODING
+print(x)
+print(y)
+print(z)
+x = "summaiya"
+y = "is"
+z = "pretty"
+print(x)
+print(y)
+print(z)
+carname = ["volvo"]
+x = carname 
+print(x)
+number_of_stars_USA =int(input("Enter the rows value for diamond"))
+for m in range(0,number_of_stars_USA):
+    for n in range(0,number_of_stars_USA-m-1):
+        print("" , end="")
+        for l in range (0,2*m+1):
+            print("*",end="")
+            print()
+            for m in range (number_of_stars_USA-1,0,-1):
+                for n in range (number_of_stars_USA,m,-1):
+                    print(" ",end="")
+                    for l in range (2*m-1,0,-1):
+                        print("*",end="")
+                        print()  
+
+ 
+
+ 
